10b01b2f763062d2b036efdfabde7dffc67d79f3
shadi <shadi9987@gmail.com>
2026-02-21 01:52:15 +0100

    Use REACT_APP_API_URL for API base URL

--- front-end/src/utils/request.js
+++ front-end/src/utils/request.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// Base Url Domain For Server
+// Base Url Domain For Server (use REACT_APP_API_URL in production)
 const request = axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000"
 });
 
 export default request;
