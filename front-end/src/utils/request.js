ab852b48ea1353a4363854ea3535cc32cb846e8e
ShadiAlhamdo <122041166+ShadiAlhamdo@users.noreply.github.com>
2026-02-21 04:20:11 +0100

    Update request.js

--- front-end/src/utils/request.js
+++ front-end/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base Url Domain For Server (use REACT_APP_API_URL in production)
 const request = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000"
+    baseURL: process.env.REACT_APP_API_URL || "https://api.shadialhamdo.com"
 });
 
 export default request;
